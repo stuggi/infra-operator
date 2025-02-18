bd55dbdcf6d134cb85f8849f876b14b353c2eba1
Luca Miccini <lmiccini@redhat.com>
2025-02-18 11:00:26 +0100

    MTLS helper functions
    - CreateMTLSVolumeMounts: add volume mount for MTLS certificates and CA certificate
    - CreateMTLSVolume: add volume for MTLS certificates and CA certificate for the service
    
    - CaMountPath, CertMountPath and KeyMountPath return strings for the respective paths to the files.

--- apis/memcached/v1beta1/memcached_funcs.go
+++ apis/memcached/v1beta1/memcached_funcs.go
@@ -23,7 +23,31 @@ import (
 	condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/helper"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/util"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+)
+
+const (
+	// CertKey - key of the secret entry holding the cert
+	CertKey = "tls.crt"
+	// PrivateKey - key of the secret entry holding the cert private key
+	PrivateKey = "tls.key"
+	// CAKey - key of the secret entry holding the CA
+	CAKey = "ca.crt"
+
+	// CaPath - path to the ca certificate (Kolla will move this)
+	CaPath = "/var/lib/config-data/mtls/certs/mtls-ca.crt"
+	// CaPathDst - path to the ca certificate
+	CaPathDst = "/etc/pki/tls/certs/mtls-ca.crt"
+	// CertPath - path to the client certificate (Kolla will move this)
+	CertPath = "/var/lib/config-data/mtls/certs/mtls.crt"
+	// CertPathDst - path to the client certificate
+	CertPathDst = "/etc/pki/tls/certs/mtls.crt"
+	// KeyPath - path to the key (Kolla will move this)
+	KeyPath = "/var/lib/config-data/mtls/private/mtls.key"
+	// KeyPathDst - path to the key
+	KeyPathDst = "/etc/pki/tls/private/mtls.key"
 )
 
 // IsReady - returns true if Memcached is reconciled successfully
@@ -114,4 +138,62 @@ func GetMemcachedByName(
 		return nil, err
 	}
 	return memcached, err
+}
+
+// CreateMTLSVolumeMounts - add volume mount for MTLS certificates and CA certificate
+func CreateMTLSVolumeMounts(SecretName string) []corev1.VolumeMount {
+	volumeMounts := []corev1.VolumeMount{}
+	if SecretName != "" {
+		volumeMounts = append(volumeMounts, corev1.VolumeMount{
+			Name:      SecretName,
+			MountPath: CertPath,
+			SubPath:   CertKey,
+			ReadOnly:  true,
+		}, corev1.VolumeMount{
+			Name:      SecretName,
+			MountPath: KeyPath,
+			SubPath:   PrivateKey,
+			ReadOnly:  true,
+		}, corev1.VolumeMount{
+			Name:      SecretName,
+			MountPath: CaPath,
+			SubPath:   CAKey,
+			ReadOnly:  true,
+		})
+	}
+
+	return volumeMounts
+}
+
+// CreateMTLSVolume - add volume for MTLS certificates and CA certificate for the service
+func CreateMTLSVolume(SecretName string) corev1.Volume {
+	volume := corev1.Volume{}
+	if SecretName != "" {
+		volume = corev1.Volume{
+			Name: SecretName,
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName:  SecretName,
+					DefaultMode: ptr.To[int32](0400),
+				},
+			},
+		}
+	}
+
+	return volume
+}
+
+// CaMountPath - returns path to the ca certificate
+func CaMountPath() string {
+	return CaPathDst
+}
+
+// CertMountPath - returns path to the certificate
+func CertMountPath() string {
+	return CertPathDst
+}
+
+// KeyMountPath - returns path to the key
+func KeyMountPath() string {
+	return KeyPathDst
 }
