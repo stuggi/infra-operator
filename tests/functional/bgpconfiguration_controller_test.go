560c69c419594862c22906234da7822c6d20f9a8
Martin Schuppert <mschuppert@redhat.com>
2025-05-09 12:25:49 +0200

    [bgp] add test to check pod FRRConfig cleanup when BGPConfig get deleted
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- tests/functional/bgpconfiguration_controller_test.go
+++ tests/functional/bgpconfiguration_controller_test.go
@@ -21,6 +21,7 @@ import (
 	. "github.com/onsi/ginkgo/v2" //revive:disable:dot-imports
 	. "github.com/onsi/gomega"    //revive:disable:dot-imports
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	frrk8sv1 "github.com/metallb/frr-k8s/api/v1beta1"
 	corev1 "k8s.io/api/core/v1"
@@ -44,6 +45,53 @@ var _ = Describe("BGPConfiguration controller", func() {
 				bgpcfg := GetBGPConfiguration(bgpcfgName)
 				g.Expect(bgpcfg).To(Not(BeNil()))
 				g.Expect(bgpcfg.Spec.FRRConfigurationNamespace).To(Equal(frrCfgNamespace))
+			}, timeout, interval).Should(Succeed())
+		})
+	})
+
+	When("a BGPConfiguration CR is deleted, finalizer logic removes owned FRRConfiguration objects", func() {
+		var podFrrName types.NamespacedName
+		var podName types.NamespacedName
+		var metallbNS *corev1.Namespace
+		var bgpcfg client.Object
+
+		BeforeEach(func() {
+			metallbNS = th.CreateNamespace(frrCfgNamespace + "-" + namespace)
+			// create a FRR configuration for a node
+			meallbFRRCfgName = types.NamespacedName{Namespace: metallbNS.Name, Name: "worker-0"}
+			meallbFRRCfg := CreateFRRConfiguration(meallbFRRCfgName, GetMetalLBFRRConfigurationSpec("worker-0"))
+			Expect(meallbFRRCfg).To(Not(BeNil()))
+
+			// create a nad config with gateway
+			nad := th.CreateNAD(types.NamespacedName{Namespace: namespace, Name: "internalapi"}, GetNADSpec())
+
+			bgpcfg = CreateBGPConfiguration(namespace, GetBGPConfigurationSpec(metallbNS.Name))
+			bgpcfgName.Name = bgpcfg.GetName()
+			bgpcfgName.Namespace = bgpcfg.GetNamespace()
+
+			podName = types.NamespacedName{Namespace: namespace, Name: uuid.New().String()}
+			// create pod without NAD annotation
+			th.CreatePod(podName, map[string]string{}, GetPodSpec("worker-0"))
+			th.SimulatePodPhaseRunning(podName)
+
+			podFrrName.Name = podName.Namespace + "-" + podName.Name
+			podFrrName.Namespace = metallbNS.Name
+
+			DeferCleanup(th.DeleteInstance, nad)
+			DeferCleanup(th.DeleteInstance, meallbFRRCfg)
+		})
+
+		It("should remove all owned FRRConfiguration objects after deletion", func() {
+			pod := th.GetPod(podName)
+			Expect(pod).To(Not(BeNil()))
+
+			// Delete the BGPConfiguration CR.
+			th.DeleteInstance(bgpcfg)
+
+			// validate that the pod frr cfg is gone
+			frr := &frrk8sv1.FRRConfiguration{}
+			Eventually(func(g Gomega) {
+				g.Expect(k8sClient.Get(ctx, podFrrName, frr)).Should(Not(Succeed()))
 			}, timeout, interval).Should(Succeed())
 		})
 	})
